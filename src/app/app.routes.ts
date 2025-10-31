ab7bdfe11fdd8099b01d680ac6fa28c09984a25d
Arjun <arjunsemwal02@gmail.com>
2025-10-31 22:06:50 +0530

    added reusable components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Housing } from './pages/housing/housing';
+import { Signup } from './pages/signup/signup';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
     {
         path: '',
         component: Dashboard
+    },
+    {
+        path: 'housing',
+        component: Housing
+    },
+    {
+        path: 'signup',
+        component: Signup
+    },
+    {
+        path: 'login',
+        component: Login
     }
 ];
